c420dad652422175a69eba93372ce9523e6fd197
dottus8 <domizianorori@gmail.com>
2025-07-19 11:30:21 +0200

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div className="p-4 text-center text-xl font-bold">
+      App di Allenamento - Benvenuto!
+    </div>
+  )
+}
